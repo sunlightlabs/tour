bb1210b636226546c5d029421a1b63686b99c5a6
Daniel Cloud <dcloud@sunlightfoundation.com>
2011-07-18 17:45:04 -0400

    Add basic logic for gelocation. MapView will now place a marker for the user's location. Scandal model given distance from method, but distance filtering not implemented. Extended Number object to have functions to convert numbers to radians or degrees

--- titanium/Scandal Tour/Resources/scandaltour/ui/MapView.js
+++ titanium/Scandal Tour/Resources/scandaltour/ui/MapView.js
@@ -4,9 +4,18 @@
  * @version $Rev$
  */
 
+ /*
+     http://developer.android.com/guide/topics/location/obtaining-user-location.html#MockData
+     Open ddms and telnet into device based on port
+     geo fix -77.04293799999999 38.906972
+ */
+
 (function() {
     st.ui.createMapView = function(_args) {
         Ti.API.log('createMapView', 'createMapView run with _args.region: ' + _args.region);
+        
+        Titanium.Geolocation.purpose = 'Find scandals near you!';
+        
         var mapView = Ti.Map.createView({
             region: _args.region,
             userLocation: true,
@@ -18,7 +27,7 @@
             Ti.API.log('MapView', "loadAnnotations()");
             var scandals = st.model.list('Scandal');
             for (var i=0; i < scandals.length; i++) {
-                Ti.API.log('MapView.loadAnnotations()', 'scandal[' + i + ']: ' + scandals[i]);
+                // Ti.API.log('MapView.loadAnnotations()', 'scandal[' + i + ']: ' + scandals[i]);
                 var annotation = Ti.Map.createAnnotation({
                     latitude: scandals[i].lat,
                     longitude: scandals[i].lon,
@@ -31,7 +40,28 @@
             }
         }
         
+        function getCurrentLocation() {
+            Ti.API.log('MapView', 'getCurrentLocation');
+            Ti.Geolocation.getCurrentPosition(function(e) {
+                if (!e.success || e.error)
+                {
+                    st.ui.alert('Gelocation Error', JSON.stringify(e.error));
+                    return;
+                }
+                var userAnnotation = Ti.Map.createAnnotation({
+                    latitude: e.coords.latitude,
+                    longitude: e.coords.longitude,
+                    title: 'Your Current Location',
+                    pincolor: Ti.Map.ANNOTATION_PURPLE,
+                    animate: false
+                });
+                mapView.addAnnotation(userAnnotation);
+                Ti.API.log('MapView', 'Gelocation Updated: ' + e.coords.latitude + ',' + e.coords.longitude);                
+            });
+        }
+        
         loadAnnotations();
+        getCurrentLocation();
         
         return mapView;
     };
