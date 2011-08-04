016253983a738fad695ccca96d4884f81363760e
Daniel Cloud <dcloud@sunlightfoundation.com>
2011-08-04 17:00:44 -0400

    Move db install action into st.model.install method. Set an App.Property so we can check the install status if need be.

--- titanium/Scandal Tour/Resources/scandaltour/model/model.js
+++ titanium/Scandal Tour/Resources/scandaltour/model/model.js
@@ -13,14 +13,15 @@
         dbname:'scandal_tour'
     };
     st.model.installed = false;
-    var db = Ti.Database.install('/scandaltour/model/scandal_tour.sqlite3', st.model.dbname);
-    db.close();
-    joli.connection = new joli.Connection(st.model.dbname);
         
     st.model.install = function() {
+        var db = Ti.Database.install('/scandaltour/model/scandal_tour.sqlite3', st.model.dbname);
+        db.close();
+        joli.connection = new joli.Connection(st.model.dbname);
         joli.models.initialize();
         Ti.API.log('st.model.install', 'Install scandal_tour');
         Ti.App.fireEvent('app:db.installed');
+        Ti.App.Properties.setBool('installed', true);
     };
     
 })();
