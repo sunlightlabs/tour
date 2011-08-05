19471fa57b16d5977f88beed7786bf802b205667
Daniel Cloud <dcloud@sunlightfoundation.com>
2011-08-05 14:06:29 -0400

    Correct 'distanceFrom' to be an objectMethod, not a class method.

--- titanium/Scandal Tour/Resources/scandaltour/model/Location.js
+++ titanium/Scandal Tour/Resources/scandaltour/model/Location.js
@@ -16,16 +16,15 @@
             longitude: 'FLOAT',
             has_visited: 'INTEGER'
         },
-        methods: {
-            distanceFrom: function(l_id, comparison_location) {
-                var that = st.model.Location.findById(l_id);
+        objectMethods: {
+            distanceFrom: function(comparison_location) {
                 var fromLat = comparison_location.latitude;
                 var fromLon = comparison_location.longitude;
 
                 var R = 3958.76; // miles
-                var dLat = (fromLat-that.latitude).toRad();
-                var dLon = (fromLon-that.lon).toRad(); 
-                var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(that.lat.toRad()) * Math.cos(fromLat.toRad()) * Math.sin(dLon/2) * Math.sin(dLon/2);
+                var dLat = (fromLat-this.latitude).toRad();
+                var dLon = (fromLon-this.longitude).toRad(); 
+                var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(this.latitude.toRad()) * Math.cos(fromLat.toRad()) * Math.sin(dLon/2) * Math.sin(dLon/2);
                 var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
                 var d = R * c;
                 return d;
