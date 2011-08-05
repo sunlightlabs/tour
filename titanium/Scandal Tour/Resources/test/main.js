6311be941836385a47087cbb8c14f888624b53a0
Daniel Cloud <dcloud@sunlightfoundation.com>
2011-08-05 18:42:30 -0400

    Add test of Titanium JavaScript virtual machine's support for ECMAScript 5/JavaScript 1.8.1 getter and setters

--- titanium/Scandal Tour/Resources/test/main.js
+++ titanium/Scandal Tour/Resources/test/main.js
@@ -19,6 +19,21 @@
             
         });
 */
+        
+        describe('JavaScript VM', function() {
+            // https://developer.mozilla.org/en/Core_JavaScript_1.5_Guide/Working_with_Objects#Defining_Getters_and_Setters
+            it('does the JavaScript Vm support JavaScript 1.8.1 getters and setters', function() {                
+                var o = {
+                    a: 7, 
+                    get b() {return this.a + 1;}, 
+                    set c(x) {this.a = x / 2}
+                };
+                expect(o.a).toBe(7);
+                expect(o.b).toBe(8);
+                o.c = 50;
+                expect(o.a).toBe(25);
+            });
+        });
 
     });
     
